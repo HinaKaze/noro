069821dc26df8ad914d29a157f8e051a6132e069
HinaKaze <xxpkq@sina.com>
2017-01-16 14:03:34 +0800

    fix bugs

--- static/js/noro.js
+++ static/js/noro.js
@@ -1,12 +1,19 @@
 class Noro {
+    constructor(){
+    	this.chatRoomId = -1;
+    	this.chatRoomWS = undefined;
+    }
     getFormatTime(t) {
         var d = new Date();
         d.setTime(Date.parse(t));
         return d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     }
-    constructor(){
-    	this.chatRoomId = -1;
-    	this.chatRoomWS = undefined;
+    sweepChatRoom(){
+    	if ($("user-dashboard-function-input").attr("display") != "none"){
+    		$("#user-dashboard-function-input").fadeOut(500);
+    	};
+        $("#user-dashboard-function-input-text").val("");
+        $("#main-content-left").html("");
     }
 }
 
