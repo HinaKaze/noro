1eb82543cfa56c3e8c5caa4f45d4c457cda5f465
HinaKaze <xxpkq@sina.com>
2017-01-22 18:58:25 +0800

    use rem

--- static/js/noro.js
+++ static/js/noro.js
@@ -1,29 +1,51 @@
 class Noro {
-    constructor(){
-    	this.chatRoomId = -1;
-    	this.chatRoomWS = undefined;
+    constructor() {
+        this.chatRoomId = -1;
+        this.chatRoomWS = undefined;
     }
     getFormatTime(t) {
         var d = new Date();
         d.setTime(Date.parse(t));
         return d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     }
-    sweepChatRoom(){
-    	if ($("user-dashboard-function-input").attr("display") != "none"){
-    		$("#user-dashboard-function-input").fadeOut(500);
-    	};
+    sweepChatRoom() {
+        if ($("user-dashboard-function-input").attr("display") != "none") {
+            $("#user-dashboard-function-input").fadeOut(500);
+        };
         $("#user-dashboard-function-input-text").val("");
         $("#main-content-left").html("");
         this.clearMainBackground();
     }
-    changeMainBackground(imgurl){
-        $("#main-content").css("background-image",'url("'+imgurl+'")');
-        $("#main-content").css("background-position","center");
-        $("#main-content").css("background-repeat","repeat-y");
+    changeMainBackground(imgurl) {
+        $("#main-content").css("background-image", 'url("' + imgurl + '")');
+        $("#main-content").css("background-position", "center");
+        $("#main-content").css("background-repeat", "repeat-y");
     }
-    clearMainBackground(){
-        $("#main-content").css("background-image","none");
+    clearMainBackground() {
+        $("#main-content").css("background-image", "none");
     }
 }
 
 var noro = new Noro();
+
+//自适应
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth >= 1920) {
+                docEl.style.fontSize = '100px';
+                 console.log("1",docEl.style.fontSize)
+            } else {
+                docEl.style.fontSize = 100 * (clientWidth / 1920) + 'px';
+                console.log("2",docEl.style.fontSize)
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
